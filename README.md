4dec001d743b0d6937670ea65a23b21398e518f1
Mark Kolich <social@kolich.com>
2015-09-26 15:35:55 -0700

    Initial commit demonstrating awesomeness.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # logback-capturing-appender
-Demonstrates how to capture Logback logging output for unit tests.
+
+This repo demonstrates how to capture Logback logging output for unit tests.
+
+Sometimes, you need to verify that a class passes unit tests *and* logs the right message at the correct logging level.
+
+Not surprisingly, this is easily achievable using a custom Logback appender in unit tests.
---
+++ src/test/java/com/kolich/logback/LogbackCapturingAppender.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2015 Mark S. Kolich
+ * http://mark.koli.ch
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.kolich.logback;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A class which demonstrates how to inject yourself into a {@link Logger} used by a specific
+ * class under test.  This allows you to intercept logging events (level and message) to verify
+ * that a specific class under test is logging the correct information and in the way that you'd
+ * expect.
+ */
+public final class LogbackCapturingAppender extends AppenderBase<ILoggingEvent> {
+
+    public static final class Factory {
+
+        private static final List<LogbackCapturingAppender> ALL = new ArrayList<>();
+
+        public static LogbackCapturingAppender attach(final org.slf4j.Logger slf4jLogger) {
+            final LogbackCapturingAppender appender = new LogbackCapturingAppender(slf4jLogger);
+            ALL.add(appender);
+            return appender;
+        }
+
+        public static void detach() {
+            ALL.forEach(LogbackCapturingAppender::detach);
+        }
+
+    }
+
+    private final Logger logger_;
+    private ILoggingEvent captured_;
+
+    private LogbackCapturingAppender(final org.slf4j.Logger slf4jLogger) {
+        logger_ = (Logger) slf4jLogger;
+        logger_.setLevel(Level.ALL);
+        logger_.addAppender(this);
+        start();
+    }
+
+    public String getCapturedLogMessage() {
+        return (captured_ == null) ? null : captured_.getMessage();
+    }
+    public Level getCapturedLogLevel() {
+        return (captured_ == null) ? null : captured_.getLevel();
+    }
+
+    @Override
+    protected void append(final ILoggingEvent event) {
+        captured_ = event;
+    }
+
+    private void detach() {
+        logger_.detachAppender(this);
+    }
+
+}
